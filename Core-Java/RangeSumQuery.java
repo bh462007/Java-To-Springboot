533aced556b5add0e9d1940fff83f49668d27c97
Bhagyashree Pal <173093839+bh462007@users.noreply.github.com>
2026-01-22 23:56:55 +0530

    Practiced array problem and fixed some issues

--- Core-Java/RangeSumQuery.java
+++ Core-Java/RangeSumQuery.java
@@ -1,10 +1,7 @@
-
-class PrefixSum{
+class RangeSumQuery{
     public static void main(String[] args) {
-        
-        int[] arr={54,23,67,89,67,12};
-        
-        int[] prefix=arr;
+        int[] arr={2,4,1,3,5};
+        int[] prefix = arr;
 
         prefix[0]=arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -14,5 +11,16 @@ class PrefixSum{
         for (int i = 0; i < prefix.length; i++) {
             System.out.println(prefix[i]);
         }
+
+        int L=1,R=3;
+        int sum=0;
+
+        if(prefix[L]==0){
+            sum=prefix[R];
+        }
+        else{
+            sum=prefix[R] - prefix[L-1];
+        }
+        System.out.println("Sum: "+sum);
     }
 }
