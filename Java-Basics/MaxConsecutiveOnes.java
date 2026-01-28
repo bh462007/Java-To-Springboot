25ae1dffd6a9c17fbaf71e214a00ee8100004e9d
Bhagyashree Pal <173093839+bh462007@users.noreply.github.com>
2026-01-28 18:25:07 +0530

    Practiced xor problem'

---
+++ Java-Basics/MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+class MaxConsecutiveOnes{
+    static int maxConsecutiveOnes(int[] nums){
+        int count=0;
+        int max=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max, count);
+            }
+            else{
+                count=0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums={1,1,0,1,1,1};
+        int k=maxConsecutiveOnes(nums);
+        System.out.println(k);
+    }
+}
