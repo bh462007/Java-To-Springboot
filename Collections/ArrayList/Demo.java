aaab2fdede44c3526013704272f93c8c09119359
Bhagyashree Pal <173093839+bh462007@users.noreply.github.com>
2026-02-22 00:12:02 +0530

    Started with arraylist

---
+++ Collections/ArrayList/Demo.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+class Demo{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        System.out.println("Before adding :");
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        System.out.println("After adding :"+arr);
+    }
+}
